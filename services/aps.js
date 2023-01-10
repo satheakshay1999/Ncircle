25c70c8628af6afdbbe87fcc39aaa659002809ed
satheakshay1999 <88333068+satheakshay1999@users.noreply.github.com>
2023-01-10 19:35:49 +0530

    Added

---
+++ services/aps.js
@@ -0,0 +1,40 @@
+service.ensureBucketExists = async (bucketKey) => {
+    try {
+        await new APS.BucketsApi().getBucketDetails(bucketKey, null, await service.getInternalToken());
+    } catch (err) {
+        if (err.response.status === 404) {
+            await new APS.BucketsApi().createBucket({ bucketKey, policyKey: 'temporary' }, {}, null, await service.getInternalToken());
+        } else {
+            throw err;
+        }
+    }
+};
+
+service.listObjects = async () => {
+    await service.ensureBucketExists(APS_BUCKET);
+    let resp = await new APS.ObjectsApi().getObjects(APS_BUCKET, { limit: 64 }, null, await service.getInternalToken());
+    let objects = resp.body.items;
+    while (resp.body.next) {
+        const startAt = new URL(resp.body.next).searchParams.get('startAt');
+        resp = await new APS.ObjectsApi().getObjects(APS_BUCKET, { limit: 64, startAt }, null, await service.getInternalToken());
+        objects = objects.concat(resp.body.items);
+    }
+    return objects;
+};
+
+service.uploadObject = async (objectName, filePath) => {
+    await service.ensureBucketExists(APS_BUCKET);
+    const buffer = await fs.promises.readFile(filePath);
+    const results = await new APS.ObjectsApi().uploadResources(
+        APS_BUCKET,
+        [{ objectKey: objectName, data: buffer }],
+        { useAcceleration: false, minutesExpiration: 15 },
+        null,
+        await service.getInternalToken()
+    );
+    if (results[0].error) {
+        throw results[0].completed;
+    } else {
+        return results[0].completed;
+    }
+};
